2f8864bc54f59bf5d539302fbd5ef45025b9a1ca
rom4uk <rsulyma86@gmail.com>
2017-09-18 14:37:23 +0300

    change script instructions, using regEx

--- scritp1.js
+++ scritp1.js
@@ -1,9 +1,58 @@
+// create form, inputs
 
+function createForm(){
+  var form = document.createElement('form');
+  form.setAttribute('name', 'login');
+  form.className = 'my-form';
+  var age = document.createElement('input');
+  age.setAttribute('type', 'text');
+  age.setAttribute('name', 'age');
+  age.className = 'inputs'
+  var username = document.createElement('input');
+  username.setAttribute('type', 'text');
+  username.setAttribute('name', 'username');
+  username.className = 'inputs'
+  var date = document.createElement('input');
+  date.setAttribute('type', 'text');
+  date.setAttribute('name', 'date');
+  date.className = 'inputs'
+  var submit = document.createElement('input');
+  submit.setAttribute('type', 'submit');
+  submit.setAttribute('value', 'Validate Me');
+  submit.className = 'inputs'
+  form.appendChild(age);
+  form.appendChild(username);
+  form.appendChild(date);
+  form.appendChild(submit);
+  document.body.insertBefore(form, document.body.firstChild);
+}
+createForm();
+
+function valid(elem){
+  var age = document.querySelector('[name=age]');
+  console.log('age', age);
+}
+
+valid();
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+   
 function validate(){
-var body = document.body; 
-var form = document.createElement('form'); 
-body.appendChild(form); 
-form.setAttribute('name', 'login'); 
+  var body = document.body; 
+  var form = document.createElement('form'); 
+  body.appendChild(form); 
+  form.setAttribute('name', 'login'); 
   function createInput(){ 
     var age = document.createElement('input');
     age.setAttribute('text', 'text');
@@ -14,28 +63,28 @@ form.setAttribute('name', 'login');
     createInput();
   }
 
-var inputs = document.forms[0];
-for(var i = 0; i < inputs.length; i++){
-  inputs[i].setAttribute('style', 'display: block; margin-top: 5px');
-  var element = inputs[i];  
-}
+  var inputs = document.forms[0];
+  for(var i = 0; i < inputs.length; i++){
+    inputs[i].setAttribute('style', 'display: block; margin-top: 5px');
+    var element = inputs[i];  
+  }
 
-inputs[0].setAttribute('placeholder', 'Вік');
-inputs[1].setAttribute('name', 'username');
-inputs[1].setAttribute('placeholder', "username begin's of user_");
-inputs[2].setAttribute('name', 'date');
-inputs[2].setAttribute('placeholder', 'Date - dd/mm/yyyy');
-inputs[3].setAttribute('type', 'submit');
-inputs[3].setAttribute('value', 'Validate Me');
-inputs[3].setAttribute('id', 'submit');
-inputs[3].removeAttribute('name');
-inputs[3].removeAttribute('text');
-var button = document.getElementById('submit');
-button.onclick = function(e){
-  e.preventDefault();
-  var z = document.querySelector('input[name="date"]');
-  var x = document.querySelector('input[name="age"]');
-  var y = document.querySelector('input[name="username"]');
+  inputs[0].setAttribute('placeholder', 'Вік');
+  inputs[1].setAttribute('name', 'username');
+  inputs[1].setAttribute('placeholder', "username begin's of user_");
+  inputs[2].setAttribute('name', 'date');
+  inputs[2].setAttribute('placeholder', 'Date - dd/mm/yyyy');
+  inputs[3].setAttribute('type', 'submit');
+  inputs[3].setAttribute('value', 'Validate Me');
+  inputs[3].setAttribute('id', 'submit');
+  inputs[3].removeAttribute('name');
+  inputs[3].removeAttribute('text');
+  var button = document.getElementById('submit');
+  button.onclick = function(e){
+    e.preventDefault();
+    var z = document.querySelector('input[name="date"]');
+    var x = document.querySelector('input[name="age"]');
+    var y = document.querySelector('input[name="username"]');
   //validate age
   if(x.value < 0){
     alert("Вік не може бути від'ємним");
@@ -76,3 +125,4 @@ document.querySelector('input[name=date]').value = '';
 };
 }
 validate();
+*/
